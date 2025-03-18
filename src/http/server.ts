0f2e35b009125243290bf53dd6f50f3649a80a6a
Matheus <matheusvitalds@gmail.com>
2025-03-18 15:13:44 -0300

    feat: create updateRing endpoint and a ring validation function

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
-import { listRings, createRings } from '../../controller/ring';
+import { listRings, createRing, updateRing } from '../../controller/ring';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', listRings);
-app.post('/', createRings);
+app.post('/', createRing);
+app.put('/:id', updateRing);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
