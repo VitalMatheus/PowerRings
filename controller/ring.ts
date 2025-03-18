6693bdebe4b912c3e67cef89b38dd2788fb1e6aa
Matheus <matheusvitalds@gmail.com>
2025-03-18 10:24:41 -0300

    feat: add create rings endpoint

--- controller/ring.ts
+++ controller/ring.ts
@@ -1,12 +1,48 @@
 import { Request, Response } from 'express';
+import { RingData } from '../types/ring';
 
 const Ring = require('../models/ring.js');
 
-export const listRings = async (req: Request, res: Response) => {
+export const listRings = async (req: Request, res: Response): Promise<any> => {
   try {
-    const rings = await Ring.findAll();
-    res.status(201).json(rings);
+    const rings: RingData = await Ring.findAll();
+    return res.status(201).json(rings);
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao listar os anéis.' });
+    return res.status(500).json({ error: 'Erro ao listar os anéis.' });
   }
 };
+
+export const createRings = async(req: Request, res: Response): Promise<any> => {
+  try {
+    const { name, power, carrier, forgedBy, image }: RingData = req.body;
+    const ringsCount = await Ring.count({ where: { forgedBy } });
+    
+    let maxRings = 0;
+    switch(forgedBy) {
+      case 'elfos':
+        maxRings = 3;
+        break;
+      case 'anões':
+        maxRings = 7;
+        break;
+      case 'homens':
+        maxRings = 9;
+        break;
+      case 'sauron':
+        maxRings = 1;
+        break
+      default:
+        return res.status(400).json({ error: 'Forjador Inválido' });
+      }
+
+      if (ringsCount >= maxRings) {
+        return res.status(400).json({ error: `Limite máximo de anéis atingido para ${forgedBy}`})
+      }
+    
+    const newRing = await Ring.create({ name, power, carrier, forgedBy, image });
+    
+    return res.status(201).json(newRing);
+  } catch (error) {
+    return res.status(500).json({ error: 'Erro ao criar o anel' });
+  }
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
-import { listRings } from '../../controller/ring';
+import { listRings, createRings } from '../../controller/ring';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', listRings);
+app.post('/', createRings);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
